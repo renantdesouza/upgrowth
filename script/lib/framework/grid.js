760f967a1637f8b26e24aa24e5b926f23d05cd32
Renan Teixeira de Souza <renantdesouza@hotmail.com>
2015-09-24 22:26:48 -0300

    grid
    grid

--- script/lib/framework/grid.js
+++ script/lib/framework/grid.js
@@ -6,4 +6,28 @@
  **/
 var UpGrowth = UpGrowth || {};
 
-UpGrowth.grid = {}/
+UpGrowth.grid = {};
+
+UpGrowth.grid.items = [];
+
+var _fields = []; 
+
+// define qual o tipo de ordenação cria.
+var order = 'crescent';
+
+UpGrowth.grid.init = function(items) {
+    this.items = items;
+    for(var i in items) {
+        // define os fields que serão o cabeçalho do grid
+        var item = items[i];
+        for(var key in item) {
+            _fields = key;
+        }
+        break;
+    }
+}; 
+
+UpGrowth.gridx.order = function(param) {
+    order = (order === 'crescent') ? 'crescent' : 'decrescent';
+    return UpGrowth.util.array[order](this.items, param);
+};
